ee990a3c934f88e060934305633a0feeb1117e50
Shiv-Chopra <shiv.chopra@sbhsstudent.school.nz>
2026-08-13 21:15:17 +0000

    Added comments and the intial import statement, which adds tkinter tools into the python code so I can build the graphical interface of the quiz.

---
+++ Quiz.py
@@ -0,0 +1,32 @@
+# -------------------------------------------
+# Southland Surfing Quiz
+# By: Shiv
+# -------------------------------------------
+
+# This program is a multiple-choice surfing quiz
+# about surfing in Southland.
+#
+# Players answer questions about:
+# - Surfing equipment
+# - Beach safety
+# - Southland surf breaks
+# - Surf etiquette
+# - Wave conditions
+# - Surfing terminology
+#
+# Players receive 10 points for each correct answer.
+
+import tkinter as tk
+from tkinter import messagebox
+
+# ------------------------------------------
+# Quiz Questions
+# ------------------------------------------
+
+# this list has all of the questions that will be used in the quiz.
+# each question has:
+# level - the difficulty of the question
+# question - the question that the player will see
+# answers - four possible answers
+# correct - the correct answer
+
